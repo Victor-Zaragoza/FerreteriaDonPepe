1fe7af31c7bce95b9a96640e7beb05cf379fb827
Sergio Solis <sergiosolishdz30@gmail.com>
2022-06-18 14:54:43 -0500

    Desing

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -8,15 +8,6 @@ import {Home} from './home.model';
 })
 export class HomeComponent implements OnInit {
 
-  recipes: Home[] = [
-    new Home('./assets/aspiradora8galones.jpg'),
-    new Home('./assets/blister-con-4-candados-de-hierro.jpg'),
-    new Home('./assets/bomba-periferica.jpg'),
-    new Home('./assets/bomba-presurizadora.jpg'),
-    new Home('./assets/bomba-sumergible-plastica.jpg'),
-    new Home('https://encrypted-tbn0gstaticcom/images?q=tbn%3AANd9GcQ_9l39e7G5y_ENz6hL6l5KhaJuroOrbzqs0Q&usqp=CAU')
-
-  ];
 
   constructor(){  
   }
